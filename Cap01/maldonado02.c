f004458001b24fa4d09590bc6badae8001c6db02
Mateo Maldonado Diaz <89480772+Mcisf@users.noreply.github.com>
2024-08-13 19:42:15 -0500

    Update maldonado02.c

--- Cap01/maldonado02.c
+++ Cap01/maldonado02.c
@@ -20,24 +20,32 @@ void bienvenida(){
 /**Funcion que sume 2 enteros**/
 void suma2enteros(){
   int prim=0, seg=0;
+  // Solicita al usuario el primer entero
   printf("Primer entero es: ");
   scanf("%d",&prim);
+  // Solicita al usuario el segundo entero
   printf("Segundo entero es: ");
   scanf("%d",&seg);
+  // Calcula la suma de los dos enteros
   int suma=prim+seg;
+  // Muestra el resultado de la suma
   printf("El resultado es: %d\n", suma);
 }
 /**Función que calcule el área de una circunferencia**/
 void areacircunferencia(){
   int radio = 0;
   float pi = 3.14;
+  // Solicita al usuario el radio de la circunferencia
   printf("El radio es: ");
   scanf("%d",&radio);
+  // Calcula el área de la circunferencia
   float area = radio*radio*pi;
+  // Muestra el resultado del cálculo del área
   printf("El area es: %f\n", area);
 }
 
 int main(int argc, char *argv[]){
+  // Llama a las funciones
   bienvenida();
   suma2enteros();
   areacircunferencia();
